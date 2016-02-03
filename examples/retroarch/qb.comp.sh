eb4d03356b4ae6fa034ddf151e4ce1130bf515e5
mikeyd <mdeguzis@gmail.com>
2016-02-03 13:40:28 -0500

    trying without globbing

--- examples/retroarch/qb.comp.sh
+++ examples/retroarch/qb.comp.sh
@@ -12,10 +12,10 @@ EOF
 
 cc_works=0
 if [ "$CC" ]; then
-	"$CC" -o "$TEMP_EXE" "$TEMP_C" && cc_works=1
+	$CC -o "$TEMP_EXE" "$TEMP_C" && cc_works=1
 else
 	for CC in ${CC:=$(which ${CROSS_COMPILE}gcc ${CROSS_COMPILE}cc ${CROSS_COMPILE}clang)} ''; do
-		"$CC" -o "$TEMP_EXE" "$TEMP_C" && cc_works=1 && break
+		$CC -o "$TEMP_EXE" "$TEMP_C" && cc_works=1 && break
 	done
 fi
 
