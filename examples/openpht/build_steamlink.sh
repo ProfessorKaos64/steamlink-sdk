63260fe785c62ca97c253265ab8fe7b3d92333d5
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-11-29 12:44:16 -0500

    Update build_steamlink.sh

--- examples/openpht/build_steamlink.sh
+++ examples/openpht/build_steamlink.sh
@@ -3,21 +3,24 @@
 
 TOP="${PWD}"
 SRC="${TOP}/openpht-src"
+RELEASE="v1.6.2.123-e23a7eef"
 
 #
 # Download the source to OpenPHT
 #
 if [[ ! -d "${SRC}" ]]; then
 
-	git clone -b "v1.6.2.123-e23a7eef" "https://github.com/RasPlex/OpenPHT.git" "${SRC}" || exit 1
+	git clone -b "${RELEASE}" "https://github.com/RasPlex/OpenPHT.git" "${SRC}" || exit 1
 	rm -f "${TOP}/.patch-applied"
 
 else
 
 	# clean and pull
 	pushd "${SRC}"
-	git clean
-	git pull
+	git checkout master
+	git clean -f
+	git pull 
+	git checkout "${RELEASE}"
 	popd
 
 fi
