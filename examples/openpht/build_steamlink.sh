4fc0166c40a6907f91f5d4a46fca89f15ac85ec1
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-11-29 22:22:08 -0500

    clear out, start simple / fresh, build from there

--- examples/openpht/build_steamlink.sh
+++ examples/openpht/build_steamlink.sh
@@ -1,83 +1,62 @@
 #!/bin/bash
 
+
+
 TOP="${PWD}"
 SRC="${TOP}/openpht-src"
 RELEASE="v1.6.2.123-e23a7eef"
 
-#
-# Download the source to OpenPHT
-#
+###################################
+# Download the sources
+###################################
+
 if [[ ! -d "${SRC}" ]]; then
 
 	git clone -b "${RELEASE}" "https://github.com/RasPlex/OpenPHT.git" "${SRC}" || exit 1
 	rm -f "${TOP}/.patch-applied"
 
-else
-
-	# clean and pull
-	pushd "${SRC}"
-	git clean -f
-	git pull origin "${RELEASE}" 
-	popd
-
 fi
 
-
-if [[ ! -f "${SRC}/configure" ]]; then
-
-	pushd "${SRC}"
-	./bootstrap || exit 1
-	popd
-
-else
-
-	pushd "${SRC}"
-	rm -f "${SRC}/configure"
-	./bootstrap || exit 1
-	popd
-
-fi
-
-
-# GMP from the Steam Link SDK conflicts with openpht
-if [ -f "${MARVELL_ROOTFS}/usr/include/gmp.h" ]; then
-	# Run this in a subshell so we don't set CC and so forth yet
-	(
-		source "${TOP}/../../setenv.sh"
-		cd "${MARVELL_SDK_PATH}/external/gmp-6.0.0"
-		if [ ! -f Makefile ]; then
-			./configure $STEAMLINK_CONFIGURE_OPTS
-		fi
-		steamlink_make_uninstall
-	)
-fi
-
-#
+###################################
 # Build OpenPHT
+###################################
 
 source "${TOP}/../../setenv.sh"
 
-export CC="$CC -DEGL_API_FB"
-export CXX="$CXX -DEGL_API_FB"
-export CFLAGS CXXFLAGS CPPFLAGS LDFLAGS
-export LD AR AS NM STRIP RANLIB OBJDUMP
-export PYTHON_VERSION PYTHON_CPPFLAGS PYTHON_LDFLAGS PYTHON_SITE_PKG PYTHON_NOVERSIONCHECK NATIVE_ROOT
-#export UDEV_CFLAGS="-I${DEPS_INSTALL_PATH}/include"
-export UDEV_LIBS=-ludev
-#export CEC_CFLAGS="-I${DEPS_INSTALL_PATH}/include"
-export CEC_LIBS=-lcec
-#export LIBXML_CFLAGS="-I${DEPS_INSTALL_PATH}/include/libxml2"
-e#xport PULSE_CFLAGS="-I${MARVELL_ROOTFS}/usr/include"
-#export PULSE_LIBS="-lpulse -L${MARVELL_ROOTFS}/usr/lib/pulseaudio -lpulsecommon-8.0"
-export PKG_CONFIG_SYSROOT_DIR="${MARVELL_ROOTFS}"
 pushd "${SRC}"
 
-# Configure
-./configure $STEAMLINK_CONFIGURE_OPTS --prefix=/home/steam/apps/openpht --disable-x11 || exit 4
+## TODO ##
+# Fix up where various vars were set in OpenPHT embedded.
+# Determine fully what is needed dep-wise
 
-################STOP################
-echo "=PAUSING FOR REVIEW="
-sleep 20
-exit 1
-################STOP################
+export PYTHON_EXEC="$SYSROOT_PREFIX/usr/bin/python2.7"
+cmake -G ninja -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+	-DENABLE_PYTHON=ON \
+	-DEXTERNAL_PYTHON_HOME="$SYSROOT_PREFIX/usr" \
+	-DPYTHON_EXEC="$PYTHON_EXEC" \
+	-DSWIG_EXECUTABLE=`which swig` \
+	-DSWIG_DIR="$ROOT/$BUILD/toolchain" \
+	-DCMAKE_PREFIX_PATH="$SYSROOT_PREFIX" \
+	-DCMAKE_LIBRARY_PATH="$SYSROOT_PREFIX/usr/lib" \
+	-DCMAKE_INCLUDE_PATH="$SYSROOT_PREFIX/usr/include;$SYSROOT_PREFIX/usr/include/python2.7" \
+	-DCOMPRESS_TEXTURES=OFF \
+	-DENABLE_DUMP_SYMBOLS=ON \
+	-DENABLE_AUTOUPDATE=ON \
+	-DTARGET_PLATFORM=LINUX \
+	-DUSE_INTERNAL_FFMPEG=OFF \
+	-DOPENELEC=ON \
+	-DENABLE_VDPAU=$ENABLE_VDPAU \
+	-DENABLE_VAAPI=$ENABLE_VAAPI \
+	-DLIRC_DEVICE=/run/lirc/lircd \
+	-DCMAKE_INSTALL_PREFIX=/usr/lib/plexht \
+	-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+	..
 
+popd
+
+###################################
+# Inststall OpenPHT
+###################################
+
+# TODO
+#  DESTDIR=$INSTALL ninja install
